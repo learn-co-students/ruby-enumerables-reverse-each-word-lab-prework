02f0da5ce4fa5193552fde1794c1f405cba46502
Janu Sung <janu.m.sung@gmail.com>
2020-02-22 08:14:20 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,12 @@
-
+def reverse_each_word(str)
+  arr = str.split(' ')
+  reversed = []
+  arr.each do |word|
+    reversed << word.reverse!
+  end 
+  reversed.join(' ')
+  
+end 
 
 def reverse_each_word(str)
   arr = str.split(' ')
